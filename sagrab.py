47e1824e2a542d2a37f3737225106ce0eae0ad7f
yestiseye <38970441+yestiseye@users.noreply.github.com>
2022-02-17 18:18:18 +1100

    Add files via upload

---
+++ sagrab.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os, sys
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+baselink = "http://nemlog.com.au/api/co2e/sa1/{0}/{1}/csv -O {2}"
+start = sys.argv[1]
+end = sys.argv[2]
+timestamp = datetime.now()
+newnames = {"SETTLEMENTDATE": "date", "DIESEL_OIL": "Distillate -  MW", "NATURAL_GAS_PIPELINE": "Gas -  MW",
+   "SOLAR": "Solar (Utility) -  MW", "WIND": "Wind -  MW", "BATTERY_DISCHARGE": "Battery (Discharging) -  MW",
+   "BATTERY_CHARGE": "Battery (Charging) -  MW", "ROOFTOP_SOLAR": "Solar (Rooftop) -  MW",
+   "NETINTERCHANGE_IMPORT": "Imports -  MW", "NETINTERCHANGE_EXPORT": "Exports -  MW"}
+
+#download source data
+savedata = "nemlog-{0}.csv".format(timestamp.strftime("%d%b%Y%H%M"))
+os.system("wget " + baselink.format(start, end, savedata))
+
+#load into python
+df = pd.read_csv(savedata)
+
+#filter out excess columns
+df = df.filter(items=newnames.keys())
+
+#covert & save (as 'output.csv')
+df.rename(newnames, axis=1, inplace=True)
+df = df[["date", "Battery (Charging) -  MW", "Exports -  MW", "Imports -  MW", "Distillate -  MW", "Gas -  MW",
+   "Battery (Discharging) -  MW", "Wind -  MW", "Solar (Utility) -  MW", "Solar (Rooftop) -  MW"]]
+df[["Temperature - C", "Emissions Intensity - kgCO₂e/MWh", "Price - AUD/MWh"]] = np.nan
+df.to_csv("output.csv", index=False)
