3d74845a153b84e280aa71d238f773438698a9b3
yestiseye <38970441+yestiseye@users.noreply.github.com>
2022-03-28 20:24:20 +1100

    Update sagrab.py

--- sagrab.py
+++ sagrab.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os, sys, urllib.request
 from datetime import datetime
 import pandas as pd
 import numpy as np
 
-baselink = "http://nemlog.com.au/api/co2e/sa1/{0}/{1}/csv -O {2}"
+#baselink = "http://nemlog.com.au/api/co2e/sa1/{0}/{1}/csv -O {2}"
+baselink = "http://nemlog.com.au/api/co2e/sa1/{0}/{1}/csv"
 start = sys.argv[1]
 end = sys.argv[2]
 timestamp = datetime.now()
@@ -18,7 +19,8 @@ newnames = {"SETTLEMENTDATE": "date", "DIESEL_OIL": "Distillate -  MW", "NATURAL
 savedata = "nemlog-{0}.csv".format(timestamp.strftime("%d%b%Y%H%M"))
 path = os.getcwd()
 fullpath = os.path.join(path, savedata)
-os.system("wget " + baselink.format(start, end, fullpath))
+#os.system("wget " + baselink.format(start, end, fullpath))
+urllib.request.urlretrieve(baselink.format(start, end), fullpath)
 
 #load into python
 df = pd.read_csv(fullpath)
